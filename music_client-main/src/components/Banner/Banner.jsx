e59d728074cc9b476f1e58f71c82f44ce9c70aa5
oscar.restrepo@gmail.com <oscar.restrepo2@gmail.com>
2022-08-31 01:23:09 -0500

    Upload

---
+++ music_client-main/src/components/Banner/Banner.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import micro from "../../assets/micro.jpg";
+import guitar from "../../assets/guitar.jpg";
+const Banner = () => {
+  return (
+    <div className="relative">
+      <Carousel
+        className=""
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={5000}>
+        <div className="text-center">
+          <div className="absolute w-full h-3/5 bg-gradient-to-t from-black to-transparent bottom-0 z-20" />
+          <img
+            src={guitar}
+            loading="lazy"
+            layout="fill"
+            alt="banner1"
+            className="w-20"
+          />
+          <h2 className="text-4xl absolute top-20 right-1/3 z-20 text-white bg-black opacity-80">
+            Your music store
+          </h2>
+        </div>
+        <div className="text-center">
+          <div className="absolute w-full h-3/5 bg-gradient-to-t from-black to-transparent bottom-0 z-20" />
+          <img
+            src={micro}
+            loading="lazy"
+            layout="fill"
+            alt="banner1"
+            className="w-20"
+          />
+          <h2 className="text-4xl absolute top-20 right-1/3 z-20 text-white bg-black opacity-80">
+            Find the best instruments
+          </h2>
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Banner;
